81a12724b0f08d9bd4b30d3d9fad4530a9b1daea
y1114039812 <1114039812@qq.com>
2018-06-04 17:20:02 +0800

    x

---
+++ lnmp/lnp_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# install nginx
+rpm -q wget || yum -y install wget 
+cd /etc/yum.repos.d
+[ -d bak ] || mkdir bak
+mv *.repo bak/
+wget http://mirrors.aliyun.com/repo/Centos-7.repo
+yum clean all;yum repolist
+cd /root
+wget http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+rpm -ivh nginx-release-centos-7-0.el7.ngx.noarch.rpm
+yum clean all;yum repolist
+yum -y install nginx
+systemctl start nginx;systemctl enable nginx
+# install php
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install php70w
+yum install -y  php70w-mysql.x86_64   php70w-gd.x86_64   php70w-ldap.x86_64   php70w-mbstring.x86_64  php70w-mcrypt.x86_64
+yum -y install php70w-fpm
+systemctl start php-fpm
+systemctl enable php-fpm
