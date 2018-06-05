f8e38cf5baaad8ef75f8eddd0d7a3217ac91ca7f
y1114039812 <1114039812@qq.com>
2018-06-06 07:47:30 +0800

    x

--- lnmp/php_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum -y install php70w
-yum install -y  php70w-mysql.x86_64   php70w-gd.x86_64   php70w-ldap.x86_64   php70w-mbstring.x86_64  php70w-mcrypt.x86_64
-yum -y install php70w-fpm
